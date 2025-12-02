e42ded8a7b9fb0a1749bf600504a304e019de7e1
AdilY04 <adil.y04@outlook.com>
2025-12-02 00:56:19 +0000

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Adil Yaseen on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
