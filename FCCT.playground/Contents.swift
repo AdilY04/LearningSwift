a971cea6ff24eb0d42a2ff4df3eb25254abe62e3
AdilY04 <adil.y04@outlook.com>
2025-12-05 08:01:22 +0000

    C6 - Closures

--- FCCT.playground/Contents.swift
+++ FCCT.playground/Contents.swift
@@ -1,22 +1,22 @@
 import Foundation
 
-func plusTwo(value: Int) {
-    let newValue = value + 2
-}
-
-plusTwo(value: 30)
-
-func newPlusTwo(value: Int) -> Int {
-    return value + 2
-}
-
-//nested functions
-func doSomethingComplicated(
-    with value: Int
-) -> Int {
-    func mainLogic(value: Int) -> Int {
-        value + 2
+let add: (Int, Int) -> Int
+    = { (lhs:Int, rhs:Int) -> Int in
+        lhs + rhs
     }
-    return mainLogic(value: value + 3)
+add(20,30)
+
+func customAdd(
+    _ lhs: Int,
+    _ rhs: Int,
+    using function: (Int, Int) -> Int
+) -> Int {
+    function(lhs, rhs)
 }
-doSomethingComplicated(with:30)
+
+// $ as a use of referring to index number of input $0 would be 20, $1 would be 30
+customAdd(20,30) {$0 + $1}
+
+let ages  = [20,30,19,40]
+ages.sorted(by: <) //sort descending
+ages.sorted(by: >) //sort ascending
