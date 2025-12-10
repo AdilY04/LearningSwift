d54937b5167024768f6b390269e10e977a971187
AdilY04 <adil.y04@outlook.com>
2025-12-10 08:53:26 +0000

    A3 - Optionals P2

--- CodeWars.playground/Contents.swift
+++ CodeWars.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+import Foundation
+/*
+1. uppercase
+ your goal is to write a function which converts the input string to uppercase.
+*/
 
-var greeting = "Hello, playground"
+func makeUpperCase(_ s: String) -> String{
+  return s.uppercased()
+}
+
+/*
+2. multiplication table for number
+ your goal is to return multiplication table for number that is always an integer from 1 to 10.
+*/
+func multi_table(_ n: Int) -> String {
+  var result: String = ""
+  for i in 1...10 {
+    result += "\(i) * \(n) = \(i*n)\n"
+  }
+  result.removeLast()
+  return result
+}
+
+/*
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+ */
+func quarter(of: Int) -> Int {
+    return (of - 1) / 3 + 1
+}
--- Tutorial.playground/Contents.swift
+++ Tutorial.playground/Contents.swift
@@ -1,3 +1,57 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//optionals in swift. needs be unwrapped (taking the value out of the optional) to be accessed. these gotta be done safely.
+
+let myBool: Bool? = false //we dont know if there is a value, if there is it is a boolean.
+//above can either be true, false or nil.
+
+//nil coalescing bool (the value of mybool, otherwise it returns default value false)
+let myOtherBool: Bool = myBool ?? false
+let userPremium: Bool? = true
+
+//if let statements
+func checkUserPremium() -> Bool {
+    //if there is a value, let newvalue equal that value
+    if let newValue = userPremium {
+        //here we have access to the non optional value
+        print(newValue)
+        return newValue
+    } else {
+        return false
+    }
+}
+
+//can also be guard let - to make sure that there is a value there.
+//otherwise return out of the function.
+
+//also covered layered if-let and layered guard statement
+
+//optional chaining
+
+func getUsername() -> String? {
+    return "test"
+}
+
+func getTitle() -> String {
+    return "title"
+}
+
+func getUserData() {
+    
+    //get the count if the user is not nil
+    let username: String? = getUsername()
+    let count: Int? = username?.count //username? tells us that this is an option
+    
+    //get the count always
+    let title: String = getTitle()
+    let count2 = title.count
+    
+    //if not nil + first char in username has a value then return value of islowercase
+    let firstCharacterIsLowercased = username?.first?.isLowercase //here is the optional chaining
+    
+    //unwrapping the optional unsafely - you gotta know optional is NOT NIL.
+    let count3: Int = username!.count
+}
+
+
+
