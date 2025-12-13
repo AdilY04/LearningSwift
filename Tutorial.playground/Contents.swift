351747262b7ae119218d83bf778d82066d04001c
AdilY04 <adil.y04@outlook.com>
2025-12-13 12:25:45 +0000

    D3 - Classes

--- Tutorial.playground/Contents.swift
+++ Tutorial.playground/Contents.swift
@@ -1,24 +1,34 @@
 import Foundation
 
-//same as struct except we know all cases at runtime
-// an enum is a type that says "a value can be one of a fixed set of options and nothing else"
+//classes - heap - reference type
 
-//base level enum
-enum AccountStatus {
-    case free
-    case trial
-    case paid
-}
-
-func describe(status: AccountStatus) -> String {
-    switch status {
-    case .free:
-        return "Free account"
-    case .trial:
-        return "Trial account"
-    case .paid:
-        return "Paid account"
+class ScreenViewModel {
+    let title: String
+    private(set) var showButton: Bool
+    
+    //classes need an explicit initaliser
+    init(title: String, showButton: Bool) {
+        self.title = title
+        self.showButton = showButton
+    }
+    
+    deinit {
+        //runs as the object is being removed from memory - specific to classes
+    }
+    
+    func hideButton() {
+        showButton = false
+    }
+    
+    func updateShowButton(newValue: Bool) {
+        showButton = newValue
     }
 }
 
-var status: AccountStatus = .paid
+//we use constants because of the reference type
+//the obj itself is not changing but the data inside it is
+let viewModel: ScreenViewModel = ScreenViewModel(title: "Screen 1", showButton: true)
+let value = viewModel.showButton
+
+viewModel.hideButton()
+viewModel.updateShowButton(newValue: false)
